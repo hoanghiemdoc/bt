81f292451ae1387bc6b4f2a3d2138fdf5dca9334
Hoang Nguyen <hoanghiemdoc231@gmail.com>
2022-07-16 15:32:12 +0700

    h

---
+++ Assigment-Sale/src/main/java/com/example/assigmentsale/AssigmentSaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.assigmentsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssigmentSaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssigmentSaleApplication.class, args);
+	}
+
+}
---
+++ Assigment-Sale/src/test/java/com/example/assigmentsale/AssigmentSaleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assigmentsale;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssigmentSaleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
